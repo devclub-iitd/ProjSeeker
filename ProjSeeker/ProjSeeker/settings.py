3e6f012a0e0e15ab4207a7002b212c965920250d
Harsh Agrawal <harsh.ag14901@gmail.com>
2020-10-24 01:20:15 +0530

    Created basic login screens

--- ProjSeeker/ProjSeeker/settings.py
+++ ProjSeeker/ProjSeeker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ProjSeeker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(os.path.join(BASE_DIR,('templates')))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- ProjSeeker/portal/urls.py
+++ ProjSeeker/portal/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from .views import index
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r"^index$", index, name="index"),
+    url('',TemplateView.as_view(template_name='home.html'),name='home'),
     
 ]
